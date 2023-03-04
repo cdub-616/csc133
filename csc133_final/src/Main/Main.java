344b05246ffd822650cc05ff3e6c79271eb5e266
cdub-616 <113482799+cdub-616@users.noreply.github.com>
2023-03-03 16:20:21 -0800

    added mouse collision and tags

--- csc133_final/src/Main/Main.java
+++ csc133_final/src/Main/Main.java
@@ -5,6 +5,8 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
+import Data.Click;
+import Data.RECT;
 import Data.Sprite;
 import logic.Control;
 import timer.stopWatchX;
@@ -12,6 +14,7 @@ import timer.stopWatchX;
 public class Main{
 	// Fields (Static) below...
 	public static String s = "";
+	public static RECT [] rs;                       //Declare array of RECT objects
 	// End Static fields...
 	
 	public static void main(String[] args) {
@@ -22,15 +25,24 @@ public class Main{
 	/* This is your access to things BEFORE the game loop starts */
 	public static void start(Control ctrl){
 		//TODO:  Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
-		BufferedImage pImage = ctrl.getSpriteFromBackBuffer("f0").getSprite();
+		rs = new RECT[3];
+		rs[0] = new RECT(46, 3, 107, 123, "Persephone");         //Persephone
+		rs[1] = new RECT(0, 0, 1279, 359, "Top space");          //Space on top half of screen
+		rs[2] = new RECT(0, 360, 1279, 719, "Bottom space");      //Space on bottom half of screen
+		//BufferedImage pImage = ctrl.getSpriteFromBackBuffer("f0").getSprite();
 	}
 	
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
 	public static void update(Control ctrl) {
-		// TODO: This is where you can code! (Starting code below is just to show you how it works)
+		// TODO: This is where you can code! (Starting code below is just to show you how it works)		
 		ctrl.addSpriteToFrontBuffer(0, 0, "f0");
 		if (Control.getMouseInput() != null) {
-			s = Control.getMouseInput().toString();
+			for (RECT r: rs) {
+				if (r.isClicked(Control.getMouseInput(), Click.LEFT_BUTTON)) {
+					s = r.getTag() + " was Clicked!";
+					break;
+				}
+			}
 		}
 		ctrl.drawString(20, 150, s, Color.WHITE);
 	}
