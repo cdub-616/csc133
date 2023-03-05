f1a22572b627b98736061e5cac668a3435ff2ffa
cdub-616 <113482799+cdub-616@users.noreply.github.com>
2023-03-04 17:25:17 -0800

    added images, RECTS, and tags

--- csc133_final/src/Main/Main.java
+++ csc133_final/src/Main/Main.java
@@ -26,24 +26,28 @@ public class Main{
 	public static void start(Control ctrl){
 		//TODO:  Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
 		rs = new RECT[3];
-		rs[0] = new RECT(46, 3, 107, 123, "Persephone");         //Persephone
-		rs[1] = new RECT(0, 0, 1279, 359, "Top space");          //Space on top half of screen
-		rs[2] = new RECT(0, 360, 1279, 719, "Bottom space");      //Space on bottom half of screen
+		rs[0] = new RECT(275, 300, 334, 416, "Persephone"); //Persephone
+		rs[1] = new RECT(571, 292, 702, 422, "tree");       //tree
+		rs[2] = new RECT(949, 294, 1033, 434, "hero");      //hero
 	}
 	
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
 	public static void update(Control ctrl) {
 		// TODO: This is where you can code! (Starting code below is just to show you how it works)		
-		ctrl.addSpriteToFrontBuffer(384, 296, "f0");
+		ctrl.addSpriteToFrontBuffer(224, 296, "f0");
+		ctrl.addSpriteToFrontBuffer(2*224 + 128, 296, "naked_tree");
+		ctrl.addSpriteToFrontBuffer(3*224 + 2*128, 296, "hero");
 		if (Control.getMouseInput() != null) {
 			for (RECT r: rs) {
 				if (r.isClicked(Control.getMouseInput(), Click.LEFT_BUTTON)) {
 					s = r.getTag() + " was Clicked!";
 					break;
 				}
+				else
+					s = "";
 			}
 		}
-		ctrl.drawString(20, 150, s, Color.WHITE);
+		ctrl.drawString(550, 150, s, Color.WHITE);
 	}
 	
 	// Additional Static methods below...(if needed)
