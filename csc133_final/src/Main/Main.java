618752a7a0614cad829f8e3fb014a737634ea357
cdub-616 <113482799+cdub-616@users.noreply.github.com>
2023-03-21 11:37:04 -0700

    chicken animation across screen

--- csc133_final/src/Main/Main.java
+++ csc133_final/src/Main/Main.java
@@ -19,6 +19,8 @@ public class Main{
 	// Fields (Static) below...
 	public static String s = "";
 	public static Animation anim;
+	public static final int screenWidth = 1280;
+	public static final int step = 12;
 	// End Static fields...
 	
 	public static void main(String[] args) {
@@ -29,9 +31,14 @@ public class Main{
 	/* This is your access to things BEFORE the game loop starts */
 	public static void start(Control ctrl){
 		//TODO:  Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
-		anim = new Animation(120, true);
-		for (int i = 0; i < 4; i++) 
-			anim.addFrame(new Frame(0, 0, "c" + i));
+		anim = new Animation(80, false);
+		int frameCounter = 0;
+		for (int x = -128; x < (screenWidth + 128); x+= step) {
+			anim.addFrame(new Frame(x, 0, "c" + frameCounter));
+			frameCounter++;
+			if (frameCounter > 3)
+				frameCounter = 0;
+		}
 	}
 	
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
@@ -40,6 +47,8 @@ public class Main{
 		Frame curFrame = anim.getCurrentFrame();
 		if (curFrame != null)
 			ctrl.addSpriteToFrontBuffer(curFrame.getX(), curFrame.getY(), curFrame.getSpriteTag());
+		if (anim.isFinished())
+			ctrl.drawString(0, 150, "Animation is finished!", Color.YELLOW);
 	}
 	
 	// Additional Static methods below...(if needed)
