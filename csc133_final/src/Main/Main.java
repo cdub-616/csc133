194915d33de81764626e643c4265a824522bdf22
cdub-616 <113482799+cdub-616@users.noreply.github.com>
2023-03-21 19:39:54 -0700

    added scripting -> Command class

--- csc133_final/src/Main/Main.java
+++ csc133_final/src/Main/Main.java
@@ -13,13 +13,16 @@ import Data.Sprite;
 import Data.Frame;
 import Input.Mouse;
 import logic.Control;
+import script.Command;
 import sound.Sound;
 import timer.stopWatchX;
 
 public class Main{
 	// Fields (Static) below...
 	//public static String coord = "";  //coordinate tool
-	
+	public static String s = "";
+	public static Command c;
+	public static Command c2;
 	// End Static fields...
 	
 	public static void main(String[] args) {
@@ -30,18 +33,29 @@ public class Main{
 	/* This is your access to things BEFORE the game loop starts */
 	public static void start(Control ctrl){
 		//TODO:  Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
-		
+		String raw = "show_sprite: 100, 100, f0";
+		String raw2 = "text: Howdy partner!";
+		c = new Command(raw);
+		c2 = new Command(raw2);
 	}
 	
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
 	public static void update(Control ctrl) {
 		// TODO: This is where you can code! (Starting code below is just to show you how it works)	
-		ctrl.addSpriteToFrontBuffer(0, 0, "f0");  //add Persephone
+		if (c.isCommand("show_sprite") && c.getNumParms() == 3) {
+			int x = Integer.parseInt(c.getParmByIndex(0));
+			int y = Integer.parseInt(c.getParmByIndex(1));
+			String tag = c.getParmByIndex(2);
+			ctrl.addSpriteToFrontBuffer(x, y, tag);
+		}
+		if (c2.isCommand("text") && c2.getNumParms() == 1) {
+			String display = c2.getParmByIndex(0);
+			ctrl.drawString(0, 250, display, Color.WHITE);
+		}
 		//Point p = Mouse.getMouseCoords();
 		//coord = p.toString();  //coordinate tool
 		//ctrl.drawString(500, 360, coord, Color.WHITE);  //coordinate tool
 	}
-	
 	// Additional Static methods below...(if needed)
 
 }
